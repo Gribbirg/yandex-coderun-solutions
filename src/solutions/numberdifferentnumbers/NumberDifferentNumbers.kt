1eb6635d7e87e0f9f7078e6afc17b5a63ba1c1aa
Alexander Gribkov <gribkovalexander@gmail.com>
2024-06-08 10:25:55 +0300

    62. number different numbers

---
+++ src/solutions/numberdifferentnumbers/NumberDifferentNumbers.kt
@@ -0,0 +1,27 @@
+package solutions.numberdifferentnumbers
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    val nums = reader.readLine().split(" ").map { it.toInt() }.toIntArray()
+
+    val res = numberDifferentNumbers(nums)
+    writer.println(res)
+
+    reader.close()
+    writer.close()
+}
+
+fun numberDifferentNumbers(nums: IntArray): Int =
+    nums.toSet().size
+
+fun BufferedWriter.println(s: Any) {
+    write(s.toString())
+    newLine()
+}
